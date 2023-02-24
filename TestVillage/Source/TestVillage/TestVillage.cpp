ea20cf811aa9dfbecbe87faff8b538fab2b99790
Viborg92 <Viborg92@hotmail.com>
2023-02-24 20:10:13 +0100

    Revert "Initial project commit"
    This reverts commit 797c5677063e0ed0ca9af5ac3f413bd9ee3662b5.

--- TestVillage/Source/TestVillage/TestVillage.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TestVillage.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestVillage, "TestVillage" );
- 
